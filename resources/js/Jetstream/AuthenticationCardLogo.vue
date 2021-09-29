61e13d26ad6bf38a41913ff8aacde4f96fcff026
owaisiqbal17 <owaisiqbal17@users.noreply.github.com>
2021-09-29 17:25:32 +0500

    Initial commit

---
+++ resources/js/Jetstream/AuthenticationCardLogo.vue
@@ -0,0 +1,18 @@
+<template>
+    <Link :href="'/'">
+        <svg class="w-16 h-16" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <img src="/logo.png">
+        </svg>
+    </Link>
+</template>
+
+<script>
+    import { defineComponent } from 'vue'
+    import { Link } from '@inertiajs/inertia-vue3';
+
+    export default defineComponent({
+        components: {
+            Link,
+        },
+    })
+</script>
